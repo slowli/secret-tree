e8115d7089220d96579ba27eea5c5ca30b0df523
Alex Ostrovski <ostrovski.alex@gmail.com>
2022-07-21 15:41:15 +0300

    Use const param for `AsByteSliceMut` impl
    ...for array types

--- src/byte_slice.rs
+++ src/byte_slice.rs
@@ -73,27 +73,15 @@ where
     }
 }
 
-macro_rules! impl_as_byte_slice_array {
-    ($($n:expr,)*) => {
-        $(
-        impl<T> AsByteSliceMut for [T; $n]
-        where
-            [T]: AsByteSliceMut,
-        {
-            fn as_byte_slice_mut(&mut self) -> &mut [u8] {
-                AsByteSliceMut::as_byte_slice_mut(&mut self[..])
-            }
+impl<T, const N: usize> AsByteSliceMut for [T; N]
+where
+    [T]: AsByteSliceMut,
+{
+    fn as_byte_slice_mut(&mut self) -> &mut [u8] {
+        AsByteSliceMut::as_byte_slice_mut(&mut self[..])
+    }
 
-            fn convert_to_le(&mut self) {
-                AsByteSliceMut::convert_to_le(&mut self[..])
-            }
-        }
-        )*
-    };
+    fn convert_to_le(&mut self) {
+        AsByteSliceMut::convert_to_le(&mut self[..]);
+    }
 }
-
-impl_as_byte_slice_array!(
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
-);
